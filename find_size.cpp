90cec6d16d5b3d85aabe5e97402072c8bc1d3d75
SATHI  RUIDAS <89245802+sathi7029@users.noreply.github.com>
2022-10-07 13:39:15 +0530

    updated

--- find_size.cpp
+++ find_size.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main() 
-{    
+{    //it will give the size of char 
     cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    //it will give the size of int 
     cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    //it will give the size of float 
     cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    //it will give the size of double
     cout << "Size of double: " << sizeof(double) << " bytes" << endl;
 
     return 0;
